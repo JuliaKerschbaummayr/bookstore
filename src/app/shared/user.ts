938fba73174440bba736cbcb7d4bc28496040b06
JuliaKerschbaummayr <s1610456015@students.fh-hagenberg.at>
2019-05-01 18:22:24 +0200

    added class user

---
+++ src/app/shared/user.ts
@@ -0,0 +1,5 @@
+export class User {
+    constructor(public id:number, public name:string, public isadmin:boolean, public email:string, public password:string, public firstname:string, public lastname:string, public street:string, public street_number:number, public zip:number, public city:string, public country:string) {
+
+    }
+}
